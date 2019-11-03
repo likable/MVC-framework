d9dace818020f41b856bafadb0ef8cff383e8b31
Виталий <wental-@mail.ru>
2019-11-04 00:29:41 +0300

    Create files structure and class Router. Add dev.php file.

---
+++ application/core/Router.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace application\core;
+
+class Router 
+{
+    protected $routes = [];
+    protected $params = [];
+    
+    public function __construct()
+    {
+        //echo "class Router";
+    }
+    
+    public function add() 
+    {
+        
+    }
+    
+    public function match() 
+    {
+        
+    }
+    
+    public function run() 
+    {
+        echo 'start';
+    }
+    
+    
+}
--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "---index.php test---";
+require 'application/lib/dev.php';
+
+use application\core\Router;
+
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class . '.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+session_start();
+
+$router = new Router();
+
+$router->run();
