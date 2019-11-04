5bfde2ac4cb6047811444bdf6c17e8d8d6e4506e
Виталий <wental-@mail.ru>
2019-11-04 16:25:38 +0300

    Filled in the __construct, add, match, run methods in Router.php class.

--- application/core/Router.php
+++ application/core/Router.php
@@ -9,22 +9,44 @@ class Router
     
     public function __construct()
     {
-        //echo "class Router";
+        $arr = require 'application/config/routes.php';
+        foreach ($arr as $key => $val) {
+            $this->add($key, $val);
+        }
     }
     
-    public function add() 
+    public function add($route, $params) 
     {
-        
+        $route = '#^' . $route . '$#';
+        $this->routes[$route] = $params;
     }
     
     public function match() 
     {
-        
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        //debug($url);
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
     }
     
     public function run() 
     {
-        echo 'start';
+        if ($this->match()) {
+            $controller = 'application\controllers\\' . ucfirst($this->params['controller']) . 'Controller.php';
+            if (class_exists($controller)) {
+                echo 'OK';
+            } else {
+                echo 'Not found: ' . $controller;
+            }
+            
+        } else {
+            echo 'Маршрут не найден';
+        }
     }
     
     
