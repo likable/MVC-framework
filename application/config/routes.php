a44002d1971ee8378e0ffc7602b3ee7cb66539aa
Виталий <wental-@mail.ru>
2019-11-05 22:41:26 +0300

    Completed class Router. Add abstract class Controller and MainController, AccountController, NewsController classes.

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,9 +1,24 @@
 <?php
 
 return [
+    '' => [
+        'controller' => 'main',
+        'action' => 'index'
+    ],
+    
+    'contacts' => [
+        'controller' => 'main',
+        'action' => 'contact'
+    ],
+    
     'account/login' => [
         'controller' => 'account',
         'action' => 'login'
+    ],
+    
+    'account/register' => [
+        'controller' => 'account',
+        'action' => 'register'
     ],
     
     'news/show' => [
